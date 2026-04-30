424eedd61a80a711b6be24bfab114ae959aae950
Mahmoud Khedr <mahmoud.s.khedr.2@gmail.com>
2026-04-30 16:17:33 +0300

    feat(chat): add AppLogger to ChatModule providers

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { AppLogger } from '../common/logging/app-logger.service';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
@@ -8,6 +9,6 @@ import { ChatWsExceptionFilter } from './chat-ws-exception.filter';
 @Module({
   imports: [JwtModule.register({})],
   controllers: [ChatController],
-  providers: [ChatService, ChatGateway, ChatWsExceptionFilter],
+  providers: [ChatService, ChatGateway, ChatWsExceptionFilter, AppLogger],
 })
 export class ChatModule {}
